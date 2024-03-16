0ec5cb888fdb5d51ce7e63f434bcb9df0e61b75b
Kirillr-Sibirski <93882929+Kirillr-Sibirski@users.noreply.github.com>
2024-03-16 12:22:05 +0100

    Add description

--- app/app/components/footer.tsx
+++ app/app/components/footer.tsx
@@ -8,9 +8,9 @@ export default function Footer() {
     return (
         <footer className="bg-white rounded-lg shadow m-4 dark:bg-bg-d">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-bg-l sm:text-center dark:text-gray-400">© 2024 <a href="#" className="hover:underline">EEL Finance™</a>. All Rights Reserved.
+            <span className="text-sm text-ac-1 sm:text-center dark:text-gray-400">© 2024 <a href="#" className="hover:underline">EEL Finance™</a>. All Rights Reserved.
             </span>
-            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-bg-l dark:text-gray-400 sm:mt-0">
+            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-ac-1 dark:text-gray-400 sm:mt-0">
                 <li>
                     <a href="https://twitter.com/EelFinance" className="hover:underline me-4 md:me-6">Twitter (X)</a>
                 </li>
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,6 +3,7 @@
 import WalletContextWrapper from "./components/walletContextWrapper";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import Container from "./components/Container";
 import Link from 'next/link';
 
 /* ------------------------ Components ----------------------- */
@@ -11,7 +12,26 @@ export default function Main() {
     <WalletContextWrapper>
       <div className="h-screen flex flex-col justify-between">
         <Header />
-          <Link href="/app">Go to Dashboard</Link>
+        <Container>
+            <div className='grid lg:grid-cols-2 mt-12 gap-8 px-4 lg:px-0 items-center justify-center'>
+              <div className='flex mb-12  flex-col justify-center'>
+                <h1 className='text-4xl lg:text-6xl font-bold mb-6'>
+                  Instant Yield Lending (IYL)
+                </h1>
+                <p className='mb-6 text-slate-400 max-w-lg lg:text-lg'>Offers users immediate access to funds without selling their crypto assets or relying on loans. Users contribute capital, receiving instant access to their desired amount. This capital is locked and invested, generating a constant APY until it covers the requested sum, providing a unique financial solution in the crypto space.</p>
+                <div>
+                <Link href='/app'>
+                  <button className="whitespace-nowrap col-span-2 text-center font-semibold rounded-md border-1 border-bg-d bg-ac-1 h-9 px-3 text-bg-d">Go to Dashboard
+                  </button>
+                </Link>
+                </div>
+
+              </div>
+              <div>
+                <img className='p-4' src='/logo.png' alt='logo-image' />
+              </div>
+          </div>
+        </Container>
         <Footer />
       </div>
     </WalletContextWrapper>
